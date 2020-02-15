acd48dca3d74c8489e626e54fa07bd4567b2c69a
AnderFreez <sia.spb.m167@gmail.com>
2020-02-15 12:12:24 +0300

    кпыфы

---
+++ ДЗ13.02.20/vowels in words.pas
@@ -0,0 +1,33 @@
+﻿Program lab8_2;
+Uses Crt;
+const
+vowels = ['a','e','i','o','u','y'];
+
+var
+s: string;
+all_words, this_word:set of char;
+i: integer;
+ch: char;
+
+begin
+clrScr;
+readln(s);
+
+all_words := vowels;
+
+i := 1;
+while i < length(s) do begin
+this_word:= [];
+
+while (i < length(s)) and not(s[i] in ['.', ',']) do begin
+if s[i] in vowels then include(this_word, s[i]);
+inc(i);
+end;
+
+inc(i);
+all_words := all_words * this_word;
+end;
+
+for ch := #0 to #255 do
+if ch in all_words then write(ch);
+end.
